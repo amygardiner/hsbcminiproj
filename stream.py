6787063581ee78cf8bb7f8e2cb72c49bd1fd8238
amygardiner <amy_rebecca_g@hotmail.co.uk>
2022-02-21 11:38:07 +0000

    Works with session token in credentials file
    Streams tape file from s3

--- stream.py
+++ stream.py
@@ -1,6 +1,6 @@
 import boto3
 from s3streaming import s3_open
 
-with s3_open('s3://group14-miniproject/lob-data/Tst2022-01-04LOBs.txt', boto_session=boto3.session.Session(aws_access_key_id="ASIA5IWSWOLR7ZHY3YEH", aws_secret_access_key="QPKxht7aJXb7n8Z/SUZVpGnaIMZIFclJZccX1ry/", aws_session_token="FwoGZXIvYXdzEOr//////////wEaDK3gvI55FzWI+QFQTSK5AatTRC9q6Gi7g/8y19LhADvQuMnckUAePtJpW+9WJkdwZXLXbTBA50J28EPz6euG6GL62dRt04DKvFXFSQ/d2H9s7aoeGG8OYGQ6M2p0cB0sCy3bidphvf5vpGrUmaGmBfizhzCrmls255vXDwCb5VDWpY+EevPZStP+PtpOWWSXO18SzJkbvhaYXWTDH7D2MpeIA8qWq/bm/jjG0cpQIQABPDvU8wH+iU6oHyFD9EKa7HOCayfac3oGKMTiyZAGMi2qR596zBrb29TzZ5Yu9Wu1cPjdnBLNXo4dVM8zjK/G6w7DOJBkfF0JPjSjQ0c=", region_name='us-east-1')) as f:
+with s3_open('s3://group14-miniproject/lob-data/Tst2022-01-04LOBs.txt', boto_session=boto3.session.Session(aws_access_key_id="ASIA5IWSWOLRSFND6MMV", aws_secret_access_key="DdBogzFG08IoPyWgrSdO0aeH4SBkhb/4IS5TDZJ7", aws_session_token="FwoGZXIvYXdzEP3//////////wEaDEuV8HOfXIMXG8tFRSK5AbFjED9vm+bavbVouC5lzTXSrf1nhVKo10HCGmM1X0lazZpvUoMrnKnO1a3PBQurC5gyR3TeIOzMrpf/2fN4TCBKbBL76zvRTCnKQuu89LVOFciwWDBW9VBAymjjN7z6x6vdnYikTQmDVDelTptGUpsisa9HcOHSfLPXiQ5a/ysDzUJDr1ZgoRvfokoMeaH+1ruzLvGCT61bU5EROLHxs/Sv/AZPk5vv6kNfVG8un0HGgJTc0greYSKLKOrqzZAGMi07uO7RXK8iVY+8f9qec/E+6S/vMI+xfXqvTqG3xmZUSeKXe+bdmUW/6xUJvYI=", region_name='us-east-1')) as f:
     for next_line in f:
         print(next_line)
