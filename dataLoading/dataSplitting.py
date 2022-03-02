9cf552ec2d49d48b3a899cf2690684c5c3808c02
Michael McCoubrey <nc21482@bristol.ac.uk>
2022-03-02 04:41:26 +0000

    fixing bugs in dataSplitting

--- dataLoading/dataSplitting.py
+++ dataLoading/dataSplitting.py
@@ -5,15 +5,14 @@ import os
 
 #opens file
 inputFilename = "Tst2022-04-29LOBs"
-
-with io.open(inputFilename + ".txt",'r', encoding='utf-8') as f:
+with io.open(inputFilename + ".txt",'r') as f: #  encoding='utf-8'
     for count, line in enumerate(f):
         if count % 1566000 == 0:
             print(count / 156600000)
         pass
-
-
 total_lines = count
+f.close()
+
 max_batch_size = 125000 #125000 file is small enough to be managed, including viewing by text editor
 total_full_iterations = math.floor((total_lines / max_batch_size))
 remaining = total_lines - (total_full_iterations * max_batch_size)
@@ -31,14 +30,19 @@ firstLine = file.readline()
 unclosedBrackets = False
 
 directory = r"C:/Users/Michael McCoubrey/Documents/DSMP/hsbcminiproj/dataLoading/"+inputFilename
-os.mkdir(directory)
+if not os.path.isdir(directory):
+    os.mkdir(directory)
+    
 for i, batch_size in enumerate(batches):
     folderName = "/sampleSubsetPart"
     filename = "sampleSubsetPart" + str(i) + ".txt"
     path = os.path.join(directory, filename)
+
     outputFile = codecs.open(path, 'w+', 'utf-16-le')
     outputFile.write(firstLine)
+
     print(i, "of", len(batches), "done")
+
     for j in range(batch_size):
         unclosedBrackets = False
         entry = []
@@ -49,6 +53,5 @@ for i, batch_size in enumerate(batches):
         for line in entry:
             if j > 0:
                 outputFile.write(line)
-
 file.close()
 outputFile.close()
