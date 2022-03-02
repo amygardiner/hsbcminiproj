bd986b66aea343aa32e90facd7e5d3cf5ad6fffe
Michael McCoubrey <nc21482@bristol.ac.uk>
2022-03-02 09:55:51 +0000

    actually adding convertLOBDataImproved this time and adding changes to analyse the performance of compressData.py

--- dataLoading/compressData.py
+++ dataLoading/compressData.py
@@ -6,23 +6,25 @@ with open(inputFilename + ".csv", mode ='r')as file:
   # reading the CSV file
   reader = csv.reader(file)
   #tabularData = [line for line in reader]
-  tabularData = list(map(lambda row: [float(row[0]), row[1], int(row[2]), int(row[3])], reader))
+  tabularData = list(map(lambda row: [int(float(row[0])/0.016), float(row[0]), row[1], int(row[2]), int(row[3])], reader))
 file.close()
-tabularData = list(map(lambda row: [float(row[0]), row[1], int(row[2]), int(row[3])], tabularData))
-
+#tabularData = list(map(lambda row: [float(row[0]), row[1], int(row[2]), int(row[3])], tabularData))
+print("data loaded")
 
 
 #we now need to the start and end time of a bid or ask
 def createEntryWithStartAndEndTime(tabularData):
-    tabularData = list(map(lambda row: [int(row[0]/0.016),row[0],row[1],row[2], row[3]], tabularData))
+    #tabularData = list(map(lambda row: [int(row[0]/0.016),row[0],row[1],row[2], row[3]], tabularData))
     initialEntryOfInterest = tabularData[0]
     startDate = tabularData[0][1]
     endDate = -1
-    
+    print("step 1 -", len(tabularData))
     rowsOfInterest = list(filter(lambda e: e[-3:] == initialEntryOfInterest[-3:], tabularData))
+    print("step 2 -", len(rowsOfInterest))
     historyOfAnEntry = []
     
     for i, row in enumerate(rowsOfInterest):
+        print(round(i/len(rowsOfInterest),2))
         if (i+1)==len(rowsOfInterest) or row[0] == rowsOfInterest[i+1][0]:
             historyOfAnEntry = historyOfAnEntry + rowsOfInterest[:i+1]
     if len(historyOfAnEntry) != 0:
@@ -30,13 +32,15 @@ def createEntryWithStartAndEndTime(tabularData):
     else:
         historyOfAnEntry.append(initialEntryOfInterest)
         endDate = -1
+    print("step 3")
     
-    tabularData = list(map(lambda row: row[1:], filter(lambda row: row not in historyOfAnEntry, tabularData)))
-    
+    tabularData = list(map(lambda row: row[:], filter(lambda row: row not in historyOfAnEntry, tabularData)))
+    print("step 4 -", len(tabularData), [startDate, endDate] + initialEntryOfInterest[2:])
     return [startDate, endDate] + initialEntryOfInterest[2:], tabularData
 
 def compressDataToStartAndEndTime(tabularData):
     data = tabularData
+    print("size of Data left to analyse:", len(data))
     
     compressed = []
     while data != []:
