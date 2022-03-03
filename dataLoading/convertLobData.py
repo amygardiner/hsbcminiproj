9747c9575faa450bc6a83438a9ff653b816f45ef
Michael McCoubrey <nc21482@bristol.ac.uk>
2022-03-03 04:25:04 +0000

    organising the branch so that it is ready to be merged to the main branch

--- dataLoading/convertLobData.py
+++ dataLoading/convertLobData.py
@@ -1,142 +1,93 @@
-import pandas as pd
-import numpy as np
-import re
 import io
+import csv
+import re
+import math
 
 #opens file
-file = io.open('sampleSubset.txt','r', encoding='utf-16-le')
+#inputFilename = "Tst2022-01-12LOBs"
+inputFilenames = [
+    #"Tst2022-01-14LOBs", # at 8034.144 there are lines of 'M's which are clearly erroneous and causeing it to crash
+    "Tst2022-01-13LOBs",
+]
 
+for inputFilename in inputFilenames:
 
-#stores data from file in array and strips whitespace
-data = [line for line in file.readlines() if re.search("[\w].*", line) != None][1:]
-file.close()
+    print("calculating number of lines")
+    with io.open(inputFilename + ".txt",'r') as lineCounter: #  encoding='utf-8'
+        for count, line in enumerate(lineCounter):
+            if count % 1566000 == 0:
+                print(count / 156600000, "complete")
+            pass
+    total_lines = count
+    lineCounter.close()
 
+    file = io.open(inputFilename + ".txt",'r', encoding='utf-8')
 
-organisedData = []
-#converts to human readable lines
-for line in data:
-    test = list(filter(lambda match: match != '' and match != ' ' and match != '\n', re.findall("\s?[\w\.]*",line)))
-    #print(test)
-    for word in list(filter(lambda match: match != '' and match != ' ' and match != '\n', re.findall("\s?[\w\.]*",line))):   
-        #removes speech marks and commas of time, bid and ask lines
-        if not("time" in word or "bid" in word or "ask" in word):
-            word = word[1:]
-        organisedData.append(word)
+    f = open(inputFilename + ".csv", 'w', newline='')
+    writer = csv.writer(f)
 
+    computationTime = round((total_lines/156696422)*19.5, 2)
+    print("starting conversion of", total_lines, "lines")
+    print("this will take roughly", computationTime, "minuites")
+
+    percentageOfLines = math.floor(total_lines*0.01)
+
+    prevState = "START"
+    timeValue = 0.0
+    transaction = ""
+    transactionValue = 0
+    transactionAmount = 0
+    for i in range(total_lines):
         
-def chunkArrayByWord(ls,chunkWord):   
-    chunkedData = []
-    chunk = []
-    for line in ls:
-        if line==chunkWord:
-            chunkedData.append(chunk)
-            chunk = []
-        else:
-            chunk.append(line)
-    return chunkedData
+        if i % percentageOfLines == 0:
+            print(round(i / total_lines,2), "complete, roughly", round(computationTime - (round(i / total_lines,2)*computationTime), 2), "minuites left")
 
-chunkedData = chunkArrayByWord(organisedData, 'time')[1:]
+        line = file.readline()
+        if re.search("[\w].*", line) != None:
+            word = list(filter(lambda match: match != '' and match != ' ' and match != '\n', re.findall("\s?[\w\.]*",line)))[0]
+            #removes speech marks and commas of time, bid and ask lines
+            if not("time" in word or "bid" in word or "ask" in word):
+                word = word[1:]   
+            
+            if prevState == "START":
+                prevState = "TIMELABEL"
+            elif prevState == "TIMELABEL":
+                timeValue = float(word)
+                prevState = "TIMEVALUE"
+            elif prevState == "TIMEVALUE":
+                prevState = "BIDLABEL"
+            elif prevState == "BIDLABEL" and word == "ask":
+                prevState = "ASKLABEL"
+            elif prevState == "BIDLABEL" and word != "ask":
+                transaction = "bid"
+                transactionValue = int(word)
+                prevState = "BIDVALUE"
+            elif prevState == "BIDVALUE":
+                transactionAmount = int(word)
+                writer.writerow([timeValue,transaction,transactionValue,transactionAmount])
+                prevState = "BIDAMOUNT"
+            elif prevState == "BIDAMOUNT" and word == "ask":
+                prevState = "ASKLABEL"
+            elif prevState == "BIDAMOUNT" and word != "ask":
+                transaction = "bid"
+                transactionValue = int(word)
+                prevState = "BIDVALUE"
+            elif prevState == "ASKLABEL" and word == "time":
+                prevState = "TIMELABEL"
+            elif prevState == "ASKLABEL" and word != "time":
+                transaction = "ask"
+                transactionValue = int(word)
+                prevState = "ASKVALUE"
+            elif prevState == "ASKVALUE":
+                transactionAmount = int(word)
+                writer.writerow([timeValue,transaction,transactionValue,transactionAmount])
+                prevState = "ASKAMOUNT"
+            elif prevState == "ASKAMOUNT" and word == "time":
+                prevState = "TIMELABEL"
+            elif prevState == "ASKAMOUNT" and word != "time":
+                transaction = "ask"
+                transactionValue = int(word)
+                prevState = "ASKVALUE"
 
-#remove entries where there are no entries in the order log book
-chunkedData = list(filter(lambda timeEntry: len(timeEntry)>3, chunkedData))
-
-evenChunkierData = []
-for time in chunkedData:
-    bidPos = 1 #it will always be 1
-    askPos = time.index('ask')
-    evenChunkierData.append([time[0], time[bidPos+1:askPos], time[askPos+1:]])
-
-organisedData = []
-for time in evenChunkierData:
-    bids = []
-    asks = []
-    for i, bid in enumerate(time[1]):
-        if i % 2 != 0:
-            bids.append(time[1][i-1:i+1])
-    for i, ask in enumerate(time[2]):
-        if i % 2 != 0:
-            asks.append(time[2][i-1:i+1])
-    organisedData.append([time[0], bids, asks])
-
-    
-# now we aim to flatten the data into tabular data which we will eventually use for the csv file
-tabularData =[]
-for time in organisedData:
-    for bids in time[1]:
-        tabularData.append([
-            float(time[0]),
-            'bid',
-            int(bids[0]),
-            int(bids[1])
-        ])
-    for asks in time[2]:
-        tabularData.append([
-            float(time[0]),
-            'ask',
-            int(asks[0]),
-            int(asks[1])
-        ])
-        
-
-if len(tabularData)>0: #its possible irrelevant data is filtered out leaving an empty list
-    # We can save this uncompressed data as csv data
-    dict = {
-        'Time': np.array(tabularData)[:,0],
-        'Transaction': np.array(tabularData)[:,1],
-        'Value': np.array(tabularData)[:,2],
-        'Amount': np.array(tabularData)[:,3],
-    }  
-
-    df = pd.DataFrame(dict)
-    df.to_csv('tabularData.csv')
-
-
-#we now need to the start and end time of a bid or ask
-def createEntryWithStartAndEndTime(tabularData):
-    tabularData = list(map(lambda row: [int(row[0]/0.016),row[0],row[1],row[2], row[3]], tabularData))
-    initialEntryOfInterest = tabularData[0]
-    startDate = tabularData[0][1]
-    endDate = -1
-    
-    rowsOfInterest = list(filter(lambda e: e[-3:] == initialEntryOfInterest[-3:], tabularData))
-    historyOfAnEntry = []
-    #historyOfAnEntry = [initialEntryOfInterest]
-    
-    for i, row in enumerate(rowsOfInterest):
-        if (i+1)==len(rowsOfInterest) or row[0] == rowsOfInterest[i+1][0]:
-            historyOfAnEntry = historyOfAnEntry + rowsOfInterest[:i+1]
-    if len(historyOfAnEntry) != 0:
-        endDate = historyOfAnEntry[-1][1]
-    else:
-        historyOfAnEntry.append(initialEntryOfInterest)
-        endDate = -1
-    
-    tabularData = list(map(lambda row: row[1:], filter(lambda row: row not in historyOfAnEntry, tabularData)))
-    
-    return [startDate, endDate] + initialEntryOfInterest[2:], tabularData
-
-def compressDataToStartAndEndTime(tabularData):
-    data = tabularData
-    
-    compressed = []
-    while data != []:
-        entry, data = createEntryWithStartAndEndTime(data)
-        compressed.append(entry)
-    
-    return np.array(compressed)
-    
-compressed = compressDataToStartAndEndTime(tabularData)
-
-
-if len(tabularData)>0: #its possible irrelevant data is filtered out leaving an empty list
-    #Now we just need to save the data as a csv file
-    dict = {
-        'Start_Time': np.array(compressed)[:,0],
-        'End_Time': np.array(compressed)[:,1],
-        'Transaction': np.array(compressed)[:,2],
-        'Value': np.array(compressed)[:,3],
-        'Amount': np.array(compressed)[:,4],
-    }  
-
-    df = pd.DataFrame(dict)
-    df.to_csv('compressedTabularData.csv') 
+    f.close()
+    file.close()
