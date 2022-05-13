c761797e57f51b6ad3884e13716dbce0afbc0b3d
amygardiner <amy_rebecca_g@hotmail.co.uk>
2022-05-13 11:35:51 +0100

    Another ARIMA version
    This uses train and test splits rather than the forecast method, with the model fitted using the training data and predictions based on the test. Shows a similar limitation as seen previously where all train data was used for predicting against actual prices before forecasting out of the dataset

--- arima.py
+++ arima.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Thu Feb 24 17:36:14 2022
+Created on Fri May 13 11:30:14 2022
 
 @author: amygardiner
 
-ARIMA model for approx. 2 min interval of tape data
+ARIMA model showing similar limitation using test-train split and predict method rather than forecasting
 """
 
 import pandas as pd
@@ -14,99 +14,28 @@ from statsmodels.graphics.tsaplots import plot_acf,plot_pacf
 from statsmodels.tsa.arima.model import ARIMA
 import matplotlib.pyplot as plt
 
-
 # Assumed the file was downloaded from S3
 df1 = pd.read_csv('Tst2022-01-04tapes.csv',usecols=[2,3],nrows=400,names=['time','value'], header=None)
+# Removing duplicate entries at given time by using mean
 df1=df1.groupby('time').mean().reset_index()
 df=df1.value
 
-# For comparing with future prediction
-df_test = pd.read_csv('Tst2022-01-04tapes.csv',usecols=[3],skiprows=399,nrows=4,names=['value'], header=None)
 
-# KPSS Test    
-result = kpss(df, regression='c', nlags="legacy")
-print('\nKPSS Statistic: %f' % result[0])
-print('p-value: %f' % result[1])
-for key, value in result[3].items():
-    print('Critial Values:')
-    print(f'   {key}, {value}')
-    
-# P value < 0.05 hence reject null hypothesis of stationarity
+# Train-test split
+df_train=df[0:230]
+df_test=df[231:282]
 
-diff=df.diff().dropna()
-
-# KPSS Test on differenced data    
-result = kpss(diff, regression='c', nlags="legacy")
-print('\nKPSS Statistic of differenced data: %f' % result[0])
-print('p-value of differenced data: %f' % result[1])
-for key, value in result[3].items():
-    print('Critial Values:')
-    print(f'   {key}, {value}')
-    
-# Hence differencing order is 1 (d) 
-# Although could be 0 with weak stationarity
-    
-# Original Series
-fig, axes = plt.subplots(2, 2, sharex=True)
-axes[0, 0].plot(df); axes[0, 0].set_title('Original Series')
-plot_acf(df, ax=axes[0, 1], lags=len(df)-1)
-
-# 1st Differencing
-axes[1, 0].plot(diff); axes[1, 0].set_title('1st Order Differencing')
-plot_acf(diff.dropna(), ax=axes[1, 1], lags=len(df)-2)
-
-# PACF plot of original and 1st differenced series
-plt.rcParams.update({'figure.figsize':(9,3), 'figure.dpi':120})
-
-fig, axes = plt.subplots(2, 2, sharex=True)
-axes[0, 0].plot(df); axes[0, 0].set_title('Original Series')
-axes[1,0].plot(diff); axes[1,0].set_title('1st Differencing')
-plot_pacf(df, ax=axes[0,1])
-plot_pacf(diff.dropna(), ax=axes[1,1])
-plt.show()
-
-# AR term 1 (p) as one lag point majorly above significance line in PACF
-# MA term 1 (q) similar in ACF
-
-# ACF plot shows overdifference so increasing MA term could remedy
-
+start = len(df_train)
+end = len(df_train) + len(df_test) - 1
 
 # 1,1,1 ARIMA Model
-arima=ARIMA(df,order=(1, 1, 1))
+arima=ARIMA(df_train,order=(1, 1, 1))
 arima_mod=arima.fit()
-print(arima_mod.summary())
-
-# 0,2,2 ARIMA Model
-arima_mod_2 = ARIMA(df, order=(0, 2, 2)).fit()
-print(arima_mod_2.summary())
-# High P>|z| for ma weight shows 1,1,1 model may be best 
-
-# Plot residual errors
-residuals = pd.DataFrame(arima_mod.resid)
-residuals = residuals[1:]
-fig, ax = plt.subplots(1,2)
-residuals.plot(title="Residuals", ax=ax[0])
-residuals.plot(kind='kde', title='Density', ax=ax[1])
-plt.show()
-# 0 mean as required
 
 # Actual vs Fitted
-arima_pred=arima_mod.predict(dynamic=False)
-arima_pred.pop(0)
-
-forecast = arima_mod.get_forecast(steps=4)
-forecast_values=forecast.predicted_mean
-forecast_values=pd.DataFrame(forecast_values)
-# Forecast converges after 4 steps, entirely after 17 
-
-print(f'The forecast values:\n{df_test}')
-print(f'The actual values:\n{forecast_values}')
-
-#plt.subplot()
-plt.figure(4)
-plt.plot(df, label="data")
-plt.plot(arima_pred, label="predictions")
-plt.plot(forecast_values, label="forecast")
-plt.title("ARIMA(1,1,1) on from t=8.192 to t=138.144s on 04/01/22")
-plt.legend()
-plt.show()
+arima_pred=arima_mod.predict(start, end,
+                             typ = 'levels').rename("Predictions")
+  
+# plot predictions and actual values
+arima_pred.plot(legend = True)
+df_test.plot(legend = True)
